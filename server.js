0b29866a6d8ce28298ab0fa4a24f9bd3138975aa
Kevinmotanya <42073382+Kevinmotanya@users.noreply.github.com>
2019-01-09 10:21:05 -0600

    serves

---
+++ server.js
@@ -0,0 +1,7 @@
+// Pull in required dependencies
+var express = require('express');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+
+var port = process.env.PORT || 8080;
+
