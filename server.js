3bb5e35fdb9606ca9c9b77b8d035c4f9e74c29f2
Kevinmotanya <42073382+Kevinmotanya@users.noreply.github.com>
2019-01-09 10:23:57 -0600

    serves

--- server.js
+++ server.js
@@ -1,7 +1,28 @@
-// Pull in required dependencies
+// the required dependencies
 var express = require('express');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
 
 var port = process.env.PORT || 8080;
 
+var app = express();
+
+// Serve up  from the 'public' dir
+app.use(express.static(process.cwd() + '/public'));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// method override
+app.use(methodOverride('_method'));
+
+// Set Handlebars and requiring the var for handlebars
+var exphbars = require('express-handlebars');
+
+app.engine('handlebars', exphbars({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+// Import routes by requiring them
+var routes = require('./controllers/burgers_controller.js');
+//using the routes
+app.use('/', routes);
+app.listen(port);
