65f0a00615a309b35795c7e2fa4d873f51608259
Kevinmotanya <42073382+Kevinmotanya@users.noreply.github.com>
2019-01-10 23:41:39 -0600

    eatburgers

--- db/schema.sql
+++ db/schema.sql
@@ -5,6 +5,6 @@ USE burgers_db;
 -- create table burgers --
 CREATE TABLE burgers (
     id INTEGER(11) AUTO_INCREMENT NOT NULL PRIMARY KEY,
-    burger_name VARCHAR(255) NOT NULL,
+    burger_name VARCHAR(250) NOT NULL,
     devoured BOOLEAN DEFAULT false
 );
