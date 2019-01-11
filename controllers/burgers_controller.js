0c8ad50ee5f5fbde2b897869a2b4688665ee0885
Kevinmotanya <42073382+Kevinmotanya@users.noreply.github.com>
2019-01-10 23:28:23 -0600

    controller js

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,66 @@
+//node dependencies
+var express = require("express");
+var router = express.Router();
+var burger = require("../models/burger");
+//route that renders all the burgers
+router.get("/", function (req, res) {
+    burger.all(function (data) {
+        var hbsObject = {
+            burgers: data
+        };
+        console.log(hbsObject)
+        res.render("index", hbsObject);
+    });
+
+});
+// (console.log(hbsObject))
+router.get("/api/burgers", function (req, res) {
+    burger.all(function (data) {
+        res.json(data);
+    });
+
+});
+//creates a new bueger
+router.post("/api/burgers", function (req, res) {
+    burger.create([
+        "burger_name", "devoured"
+    ], [
+        req.body.burger_name, req.body.devoured
+    ], function (result) {
+
+        res.json({
+            id: result.insertId
+        })
+    })
+});
+//route to update status/conditon of (a) burger(s)
+router.put("/api/burgers/:id", function (req, res) {
+    var condition = "id = " + req.params.id;
+
+    console.log("condition", condition);
+
+    burger.update({
+        devoured: req.body.devoured
+    }, condition, function (result) {
+        if (result.changedRows == 0) {
+            return res.status(404).end();
+        } else {
+            // if connections is successful
+            res.status(200).end();
+        }
+    });
+});
+//eat or delete an eaten burger
+router.delete("/api/burgers", function (req, res) {
+    var condition = "devoured = 1";
+
+    burger.delete(
+        condition,
+        function (result) {
+            console.log(result);
+            res.status(200).end();
+        }
+    )
+})
+//exporting the routes
+module.exports = router;
---
+++ db/schema.sql
@@ -0,0 +1,10 @@
+-- create database burgers_db --
+CREATE DATABASE burgers_db;
+
+USE burgers_db;
+-- create table burgers --
+CREATE TABLE burgers (
+    id INTEGER(11) AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    burger_name VARCHAR(255) NOT NULL,
+    devoured BOOLEAN DEFAULT false
+);
