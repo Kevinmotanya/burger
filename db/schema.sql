b0f55e1841fbf1ea1646f50ca3029d7fa0a6e716
Kevinmotanya <42073382+Kevinmotanya@users.noreply.github.com>
2019-02-16 13:56:49 -0600

    l

--- db/schema.sql
+++ db/schema.sql
@@ -1,7 +1,6 @@
 -- create database burgers_db --
-CREATE DATABASE burgers_db;
 
-USE burgers_db;
+USE jtry5ysukxte2mdc;
 -- create table burgers --
 CREATE TABLE burgers (
     id INTEGER(11) AUTO_INCREMENT NOT NULL PRIMARY KEY,
